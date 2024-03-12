bc5c258720ff06842325df0d54da56274bb3a4e6
Ali Bahari <alibahari007@gmail.com>
2024-03-12 16:33:53 +0330

    Add shell scripts for ProxyHotspot

--- ProxyHotspot/iptables-proxify.sh
+++ ProxyHotspot/iptables-proxify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo iptables -t nat -A OUTPUT -p tcp ! --dport 8569 -j REDIRECT --to-ports 12345
 sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5313
 sudo iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDIRECT --to-ports 5313
